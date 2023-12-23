317eca2e3c665d4b9825be19de679471cc15ae4f
Ali <alicode6.cs@gmail.com>
2023-12-23 18:24:20 +0500

    the later half of this project is also completed, used gsap library for map ranging and more animations

--- Box movement/script.js
+++ Box movement/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('mousemove', function(i){
-    gsap.to("#box", {
-        left: i.clientX + 'px',
+    let box = document.querySelector('#box').getBoundingClientRect(); //this will later help in keeping the box in bound with the visible part of the screen upon moving it
+    let restrictedMovementOfBox = gsap.utils.mapRange(0, window.innerWidth, 200 + box.width/2, window.innerWidth-(200 + box.width/2), i.clientX); //this helps in converting the whole clientX for box, diff to that of cursore
+    gsap.to("#box", { //this is all about movement of the box
+        left: restrictedMovementOfBox + 'px',
         ease: Power3
     })
 });
